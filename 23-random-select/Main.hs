36bcc66c4b9286ff2dae1bd841727a73d2114b51
pavelmukhin <p.mukhin@newmotion.com>
2021-03-24 00:16:00 +0300

    Little refactoring

--- 23-random-select/Main.hs
+++ 23-random-select/Main.hs
@@ -7,7 +7,8 @@ rndSelect _ 0 = return []
 rndSelect xs n =
     sequence $ replicate n getCh
     where 
-        getCh = fmap (\g -> let idx = (head $ take 1 (randoms g :: [Int])) `mod` (length xs) in xs !! idx) newStdGen
+        getRnd g = (head $ take 1 (randoms g :: [Int]))
+        getCh = fmap (\g -> let idx = (getRnd g) `mod` (length xs) in xs !! idx) newStdGen
 
 main :: IO ()
 main = rndSelect "abcdefgh" 3 >>= putStrLn
